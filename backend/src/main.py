3b7296c62a65fe9bd3aec66240aa614016a4d047
German <gercha2@gmail.com>
2025-05-06 08:02:50 -0500

    Fist main app for Chatbot endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,55 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from mangum import Mangum
+from flask import Flask, request, render_template
+#from flask_cors import CORS
+import json
 
-app = FastAPI(title="El Camino API")
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+app = Flask(__name__, template_folder="../../frontend/templates", static_folder="../../frontend/static")
+#CORS(app)
 
 # Configuración de CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:3000"],  # Frontend URL
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@app.get("/")
-async def root():
-    return {"message": "Bienvenido a El Camino API"}
+model_name = "facebook/blenderbot-400M-distill"
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+conversation_history = []
 
-# Handler para AWS Lambda
-handler = Mangum(app)
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/chatbot', methods=['POST'])
+def handle_prompt():
+    # Read prompt from HTTP request body
+    data = request.get_data(as_text=True)
+    data = json.loads(data)
+    input_text = data['prompt']
+
+    # Create conversation history string
+    history = "\n".join(conversation_history)
+
+    # Tokenize the input text and history
+    inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
+
+    # Generate the response from the model
+    outputs = model.generate(**inputs, max_length= 60)  # max_length will acuse model to crash at some point as history grows
+
+    # Decode the response
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+
+    # Add interaction to conversation history
+    #conversation_history.append(input_text)
+    conversation_history.append(response)
+
+    return response
+
+if __name__ == '__main__':
+   #app.run(host='0.0.0.0', port=5000)
+   app.run(debug=True, port=5000)
